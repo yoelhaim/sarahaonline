4911c7afa8e9b0223600523f0087cedae7112f49
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-05-09 02:10:42 +0000

    add darkmode and rtl true supported arabic

--- public/saraha/src/plugins/vuetify.js
+++ public/saraha/src/plugins/vuetify.js
@@ -1,7 +1,37 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
+const opts = {};
 
 export default new Vuetify({
+  opts,
+  rtl: true,
+  theme: {
+    dark: true,
+    themes: {
+      dark: {
+        primary: "#008DBD",
+        secondary: "#424242",
+        accent: "#82B1FF",
+        error: "#FF5252",
+        info: "#2196F3",
+        success: "#4CAF50",
+        warning: "#FFC107",
+        miro: "#1E1E1E",
+        colo: "#e8e8e8",
+      },
+      light: {
+        primary: "#008DBD",
+        secondary: "#424242",
+        accent: "#82B1FF",
+        error: "#FF5252",
+        info: "#2196F3",
+        success: "#4CAF50",
+        warning: "#FFC107",
+        miro: "#e8e8e8",
+        colo: "#1E1E1E",
+      },
+    },
+  },
 });
