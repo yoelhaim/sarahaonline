d560551e652c4a32c6cd0f640d79bf96e99bccba
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-05-09 02:10:06 +0000

    login page

--- public/saraha/src/views/Home.vue
+++ public/saraha/src/views/Home.vue
@@ -1,15 +1,33 @@
 <template>
-  <hello-world />
+  <v-container class="mt-5">
+    <v-row class="text-center">
+      <div
+        style="width: 500px;
+  margin: 0 auto;"
+      >
+        <v-card class="col-md-5" outlined shaped>
+          <h2>Login</h2>
+          <div class="text-center">
+            <v-form>
+              <v-col cols="12" sm="12">
+                <v-text-field
+                  v-model="message4"
+                  label="Outlined"
+                  outlined
+                  clearable
+                  prepend-inner-icon="mdi-map-marker"
+                ></v-text-field>
+              </v-col>
+            </v-form>
+          </div>
+        </v-card>
+      </div>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
-  }
+export default {
+  name: "Home",
+};
 </script>
