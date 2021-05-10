5f61a350244fec40792ed54e068d08767946f113
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-05-10 17:33:14 +0000

    add  socail  icons

--- public/saraha/src/components/navBar/nav.vue
+++ public/saraha/src/components/navBar/nav.vue
@@ -1,19 +1,32 @@
 <template>
   <div>
-    <v-app-bar app dark>
+    <v-app-bar app color="navbar">
       <div class="d-flex align-center">
         <router-link to="/">
-          <v-app-bar-title>
+          <!-- <v-app-bar-title>
             <h4 id="logo">إعترف لي</h4>
-          </v-app-bar-title></router-link
-        >
+          </v-app-bar-title> -->
+          <v-img
+            src="https://i.pinimg.com/originals/41/28/2b/41282b58cf85ddaf5d28df96ed91de98.png"
+            width="60"
+            height="60"
+          />
+        </router-link>
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn href="" target="_blank" text>
-        <span class="mr-2"></span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-btn small text fab class="mr-2 btnColor">
+        <v-icon>mdi-facebook-messenger</v-icon>
+      </v-btn>
+      <v-btn small text fab class="mr-2 btnColor">
+        <v-icon>mdi-puzzle-heart</v-icon>
+      </v-btn>
+      <v-btn small text fab class="mr-2 btnColor">
+        <v-icon>mdi-youtube</v-icon>
+      </v-btn>
+      <v-btn small text fab class="mr-2 btnColor">
+        <v-icon>mdi-cogs</v-icon>
       </v-btn>
     </v-app-bar>
   </div>
