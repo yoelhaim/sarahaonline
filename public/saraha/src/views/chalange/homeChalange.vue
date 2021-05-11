321cae12729bc1496e5671387e28a84f405b8429
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-05-11 02:47:21 +0000

    update disgin

--- public/saraha/src/views/chalange/homeChalange.vue
+++ public/saraha/src/views/chalange/homeChalange.vue
@@ -2,6 +2,23 @@
   <v-container class="mt-5">
     <v-row class="text-center">
       <div class="auth col-md-6 homePage">
+        <v-card class="col-md-5 mt-3 " outlined color="card">
+          <v-col cols="12" sm="12" md="12" class="ma-0 pa-0">
+            <v-text-field
+              label="رابطك  "
+              placeholder="رابطك  "
+              filled
+              rounded
+              dense
+              readonly
+            ></v-text-field>
+          </v-col>
+          <div>
+            <v-btn text class="mr-2 primary" rounded>
+              <v-icon>mdi-content-copy</v-icon> نسخ الرابط
+            </v-btn>
+          </div>
+        </v-card>
         <v-card
           v-for="item in 7"
           :key="item"
@@ -14,8 +31,24 @@
               <v-icon :size="40"> mdi-account-circle</v-icon>
               <strong> غير معروف</strong>
             </p>
-            <v-card class="pa-2">
-              sjsjsjskshjkshjsh
+            <v-card class="pa-2 mt-2" v-for="itt in 5" :key="itt">
+              <v-chip color="dark "
+                ><v-icon :size="15">mdi-format-align-right</v-icon></v-chip
+              >
+              <v-chip color="dark mr-2"
+                ><strong>ssbshsgdgdjhgdhjdgjkdguidt ?</strong>
+              </v-chip>
+            </v-card>
+            <v-card class="pa-2 mt-2">
+              <v-chip color="red "
+                ><v-icon :size="15">mdi-format-align-right</v-icon></v-chip
+              >
+              <v-chip color="orange mr-2"
+                ><strong
+                  >إجابة صديقك الصحيحة هي :
+                  <span style="color:black">534</span></strong
+                >
+              </v-chip>
             </v-card>
             <v-divider class="ma-2"></v-divider>
             <div>
@@ -40,7 +73,7 @@
 </template>
 
 <script>
-import pub from "../components/home/pub.vue";
+import pub from "@/components/home/pub.vue";
 export default {
   name: "Home",
   components: {
