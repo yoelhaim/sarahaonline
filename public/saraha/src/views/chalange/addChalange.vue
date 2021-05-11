411b78b1af12af64af7308d17c1b4f7d78f6221c
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-05-11 00:08:39 +0000

    disign add  quiz

---
+++ public/saraha/src/views/chalange/addChalange.vue
@@ -0,0 +1,247 @@
+<template>
+  <v-container class="mt-5">
+    <v-row class="text-center">
+      <div class="auth col-md-6 homePage">
+        <v-form>
+          <v-card class="col-md-5 mt-3 pb-1" outlined color="card">
+            <div class="text-right">
+              <v-col cols="12" sm="12" md="12" class="ma-0 pa-0">
+                <v-text-field
+                  label="اكتب سؤالك 1  "
+                  placeholder="اكتب سؤالك 1  "
+                  filled
+                  rounded
+                  dense
+                ></v-text-field>
+              </v-col>
+              <v-row>
+                <v-col cols="12" sm="6" md="4"
+                  ><v-text-field
+                    label="اكتب الجواب الصحيح  "
+                    placeholder="اكتب الجواب الصحيح  "
+                    filled
+                    rounded
+                    dense
+                  ></v-text-field
+                ></v-col>
+                <v-col cols="12" sm="6" md="4"
+                  ><v-text-field
+                    label="اكتب الجواب الخطأ 1  "
+                    placeholder="اكتب الجواب الخطأ 1  "
+                    filled
+                    rounded
+                    dense
+                  ></v-text-field
+                ></v-col>
+                <v-col cols="12" sm="6" md="4"
+                  ><v-text-field
+                    label="اكتب الجواب الخطأ 2  "
+                    placeholder="اكتب الجواب الخطأ 2  "
+                    filled
+                    rounded
+                    dense
+                  ></v-text-field
+                ></v-col>
+              </v-row>
+            </div>
+          </v-card>
+          <!-- quiz 2 -->
+          <v-card class="col-md-5 mt-3 pb-1" outlined color="card">
+            <div class="text-right">
+              <v-col cols="12" sm="12" md="12" class="ma-0 pa-0">
+                <v-text-field
+                  label="اكتب سؤالك 2  "
+                  placeholder="اكتب سؤالك 2  "
+                  filled
+                  rounded
+                  dense
+                ></v-text-field>
+              </v-col>
+              <v-row>
+                <v-col cols="12" sm="6" md="4"
+                  ><v-text-field
+                    label="اكتب الجواب الصحيح  "
+                    placeholder="اكتب الجواب الصحيح  "
+                    filled
+                    rounded
+                    dense
+                  ></v-text-field
+                ></v-col>
+                <v-col cols="12" sm="6" md="4"
+                  ><v-text-field
+                    label="اكتب الجواب الخطأ 1  "
+                    placeholder="اكتب الجواب الخطأ 1  "
+                    filled
+                    rounded
+                    dense
+                  ></v-text-field
+                ></v-col>
+                <v-col cols="12" sm="6" md="4"
+                  ><v-text-field
+                    label="اكتب الجواب الخطأ 2  "
+                    placeholder="اكتب الجواب الخطأ 2  "
+                    filled
+                    rounded
+                    dense
+                  ></v-text-field
+                ></v-col>
+              </v-row>
+            </div>
+          </v-card>
+          <!-- quiz 3 -->
+          <v-card class="col-md-5 mt-3 pb-1" outlined color="card">
+            <div class="text-right">
+              <v-col cols="12" sm="12" md="12" class="ma-0 pa-0">
+                <v-text-field
+                  label="اكتب سؤالك 3  "
+                  placeholder="اكتب سؤالك 3  "
+                  filled
+                  rounded
+                  dense
+                ></v-text-field>
+              </v-col>
+              <v-row>
+                <v-col cols="12" sm="6" md="4"
+                  ><v-text-field
+                    label="اكتب الجواب الصحيح  "
+                    placeholder="اكتب الجواب الصحيح  "
+                    filled
+                    rounded
+                    dense
+                  ></v-text-field
+                ></v-col>
+                <v-col cols="12" sm="6" md="4"
+                  ><v-text-field
+                    label="اكتب الجواب الخطأ 1  "
+                    placeholder="اكتب الجواب الخطأ 1  "
+                    filled
+                    rounded
+                    dense
+                  ></v-text-field
+                ></v-col>
+                <v-col cols="12" sm="6" md="4"
+                  ><v-text-field
+                    label="اكتب الجواب الخطأ 2  "
+                    placeholder="اكتب الجواب الخطأ 2  "
+                    filled
+                    rounded
+                    dense
+                  ></v-text-field
+                ></v-col>
+              </v-row>
+            </div>
+          </v-card>
+          <!-- quiz 4 -->
+          <v-card class="col-md-5 mt-3 pb-1" outlined color="card">
+            <div class="text-right">
+              <v-col cols="12" sm="12" md="12" class="ma-0 pa-0">
+                <v-text-field
+                  label="اكتب سؤالك 4  "
+                  placeholder="اكتب سؤالك 4  "
+                  filled
+                  rounded
+                  dense
+                ></v-text-field>
+              </v-col>
+              <v-row>
+                <v-col cols="12" sm="6" md="4"
+                  ><v-text-field
+                    label="اكتب الجواب الصحيح  "
+                    placeholder="اكتب الجواب الصحيح  "
+                    filled
+                    rounded
+                    dense
+                  ></v-text-field
+                ></v-col>
+                <v-col cols="12" sm="6" md="4"
+                  ><v-text-field
+                    label="اكتب الجواب الخطأ 1  "
+                    placeholder="اكتب الجواب الخطأ 1  "
+                    filled
+                    rounded
+                    dense
+                  ></v-text-field
+                ></v-col>
+                <v-col cols="12" sm="6" md="4"
+                  ><v-text-field
+                    label="اكتب الجواب الخطأ 2  "
+                    placeholder="اكتب الجواب الخطأ 2  "
+                    filled
+                    rounded
+                    dense
+                  ></v-text-field
+                ></v-col>
+              </v-row>
+            </div>
+          </v-card>
+          <!-- quiz 5 -->
+          <v-card class="col-md-5 mt-3 pb-1" outlined color="card">
+            <div class="text-right">
+              <v-col cols="12" sm="12" md="12" class="ma-0 pa-0">
+                <v-text-field
+                  label="اكتب سؤالك 5  "
+                  placeholder="اكتب سؤالك 5  "
+                  filled
+                  rounded
+                  dense
+                ></v-text-field>
+              </v-col>
+              <v-row>
+                <v-col cols="12" sm="6" md="4"
+                  ><v-text-field
+                    label="اكتب الجواب الصحيح  "
+                    placeholder="اكتب الجواب الصحيح  "
+                    filled
+                    rounded
+                    dense
+                  ></v-text-field
+                ></v-col>
+                <v-col cols="12" sm="6" md="4"
+                  ><v-text-field
+                    label="اكتب الجواب الخطأ 1  "
+                    placeholder="اكتب الجواب الخطأ 1  "
+                    filled
+                    rounded
+                    dense
+                  ></v-text-field
+                ></v-col>
+                <v-col cols="12" sm="6" md="4"
+                  ><v-text-field
+                    label="اكتب الجواب الخطأ 2  "
+                    placeholder="اكتب الجواب الخطأ 2  "
+                    filled
+                    rounded
+                    dense
+                  ></v-text-field
+                ></v-col>
+              </v-row>
+            </div>
+            <v-card class="pa-2">
+              <v-btn text class="mr-2 primary" rounded>
+                <v-icon>mdi-plus</v-icon> انشئ إستطلاع لاصدقائك
+              </v-btn>
+            </v-card>
+          </v-card>
+        </v-form>
+      </div>
+      <pub />
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import pub from "@/components/home/pub.vue";
+export default {
+  name: "Home",
+  components: {
+    pub,
+  },
+};
+</script>
+<style>
+.inputFormAuth {
+  padding: 4px;
+  margin: 4px;
+  height: 70px;
+}
+</style>
