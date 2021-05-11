c043074c3418ebec4b358b0982138c657ba80781
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-05-11 00:08:13 +0000

    disign input link

--- public/saraha/src/views/Home.vue
+++ public/saraha/src/views/Home.vue
@@ -1,10 +1,24 @@
 <template>
   <v-container class="mt-5">
     <v-row class="text-center">
-      <div
-        style="width: 500px;
-  margin: 0 auto;"
-      >
+      <div class="auth col-md-6 homePage">
+        <v-card class="col-md-5 mt-3 " outlined color="card">
+          <v-col cols="12" sm="12" md="12" class="ma-0 pa-0">
+            <v-text-field
+              label="رابطك  "
+              placeholder="رابطك  "
+              filled
+              rounded
+              dense
+              readonly
+            ></v-text-field>
+          </v-col>
+          <div>
+            <v-btn text class="mr-2 primary" rounded>
+              <v-icon>mdi-content-copy</v-icon> نسخ الرابط
+            </v-btn>
+          </div>
+        </v-card>
         <v-card
           v-for="item in 7"
           :key="item"
@@ -14,7 +28,7 @@
         >
           <div class="text-right">
             <p>
-              <v-icon> mdi-account-circle</v-icon>
+              <v-icon :size="40"> mdi-account-circle</v-icon>
               <strong> غير معروف</strong>
             </p>
             <v-card class="pa-2">
@@ -22,25 +36,33 @@
             </v-card>
             <v-divider class="ma-2"></v-divider>
             <div>
-              <v-chip color="orange"
+              <v-chip color="dark" style="font-size:11px"
                 ><v-icon :size="15">mdi-clock</v-icon> ! minit</v-chip
               >
               <span class="float-left">
-                <v-chip color="red"
+                <v-chip color="success"
+                  ><v-icon :size="15">mdi-download</v-icon> حفض</v-chip
+                >
+                <v-chip color="primary" class="mr-1" style="font-size:11px"
                   ><v-icon :size="15">mdi-share</v-icon> مشاركة</v-chip
-                ></span
-              >
+                >
+              </span>
             </div>
           </div>
         </v-card>
       </div>
+      <pub />
     </v-row>
   </v-container>
 </template>
 
 <script>
+import pub from "../components/home/pub.vue";
 export default {
   name: "Home",
+  components: {
+    pub,
+  },
 };
 </script>
 <style>
