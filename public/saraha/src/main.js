c6ec9204abf69454f5f38b7e1b2d07540f00a038
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-05-11 02:45:14 +0000

    include patterns in main

--- public/saraha/src/main.js
+++ public/saraha/src/main.js
@@ -2,11 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import "./assets/css/style.css";
+import patterns from "./store/patterns";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
+Vue.prototype.$pattern = patterns;
 
 new Vue({
   router,
