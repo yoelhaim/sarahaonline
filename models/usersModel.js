da2669c4ffd6d3aab313b9fa52f07fee0c70cc81
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-05-12 22:35:32 +0000

    complete users table

--- models/usersModel.js
+++ models/usersModel.js
@@ -4,5 +4,25 @@ const users = (db, dtype) => {
       type: type.STRING,
       allowNull: false,
     },
+    password: {
+      type: type.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: type.STRING,
+      allowNull: false,
+    },
+    img_profile: {
+      type: type.STRING,
+      allowNull: false,
+    },
+    link_profile: {
+      type: type.STRING,
+      allowNull: false,
+    },
+    fcm: {
+      type: type.STRING,
+      // allowNull: false,
+    },
   });
 };
