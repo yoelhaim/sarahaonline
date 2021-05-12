fd0e17c58c0af6b60a4ef5615dd527d05528618b
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-05-12 23:22:34 +0000

    add routes in  server

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express();
 const port = 3000;
 
 // include routes
-
+const UserRoute = require("./route/userRoute");
 // fin include roues
+app.use("/auth", UserRoute);
 
 app.listen(port, () => console.log("success connect "));
