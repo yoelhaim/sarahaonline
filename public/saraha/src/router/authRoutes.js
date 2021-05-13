8eb6e984d35a2a525e87b53e4cfdc61668e624c1
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-05-13 23:28:04 +0000

    include loginauth

--- public/saraha/src/router/authRoutes.js
+++ public/saraha/src/router/authRoutes.js
@@ -3,10 +3,16 @@ export default [
     path: "/signin",
     name: "SignIn",
     component: () => import("../views/auth/signIn.vue"),
+    meta: {
+      authRequired: false,
+    },
   },
   {
     path: "/signup",
     name: "SignUp",
     component: () => import("../views/auth/signUp.vue"),
+    meta: {
+      authRequired: false,
+    },
   },
 ];
