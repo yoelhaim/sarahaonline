fd401f04877721162b7ac9b5f2902259dea832c1
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-05-13 23:28:57 +0000

    update name

--- route/userRoute.js
+++ route/userRoute.js
@@ -1,6 +1,7 @@
 const routes = require("express").Router();
-const { addusers } = require("../controller/userController");
+const { addusers, signIn } = require("../controller/userController");
 
 routes.post("/add", addusers);
+routes.post("/signin", signIn);
 
 module.exports = routes;
