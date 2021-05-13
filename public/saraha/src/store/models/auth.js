3cb6e481ec780864a29714c15c7b73cb985b0067
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-05-13 23:28:19 +0000

    systeme login front end

--- public/saraha/src/store/models/auth.js
+++ public/saraha/src/store/models/auth.js
@@ -1,4 +1,5 @@
 import Vue from "vue";
+import router from "../../router";
 // import router from "../../router/index";
 
 export default {
@@ -14,33 +15,64 @@ export default {
     getuserId: (state) => state.user.id,
     getfullName: (state) => state.user.fullname,
     getEmail: (state) => state.user.email,
+    getLink: (state) => state.user.link,
   },
   mutations: {
     AUTH_SUCCESS: (state, payload) => {
       state.accessToken = payload.accessToken;
       state.user = payload.user;
-      localStorage.getItem("access_token", payload.accessToken);
-      localStorage.setItem("user", JSON.parse(payload.user));
+      console.log("hehe " + JSON.parse(payload.user));
+      localStorage.setItem("access_token", payload.accessToken);
+      localStorage.setItem("user", payload.user);
+    },
+    logOut_session: (state) => {
+      localStorage.removeItem("access_token");
+      localStorage.removeItem("user");
+      state.accessToken = "";
+      state.user = {};
     },
   },
   actions: {
+    signIn: (context, pay) => {
+      return new Promise((resolve, reject) => {
+        Vue.prototype.$http
+          .post("/auth/signin", pay)
+          .then(async (res) => {
+            if (res.status === 200 && res.data.code === 200) {
+              await context.commit("AUTH_SUCCESS", {
+                accessToken: res.data.token,
+                user: JSON.stringify(res.data),
+              });
+              resolve(res);
+            }
+          })
+          .catch((err) => reject(err));
+      });
+    },
     signUp: (context, payload) => {
       return new Promise((resolve, reject) => {
         Vue.prototype.$http
           .post("/auth/add", payload)
           .then(async (res) => {
             if (res.status === 200) {
-              await context.commit("AUTH_SUCCESS", {
-                accessToken: "sjsjsj",
-                user: res.data.data,
-              });
+              if (res.data.code === 200) {
+                await context.commit("AUTH_SUCCESS", {
+                  accessToken: res.data.token,
+                  user: JSON.stringify(res.data),
+                });
+              }
+
+              resolve(res);
             }
-            resolve(res);
           })
           .catch((err) => {
             reject(err);
           });
       });
+    },
+    signOut: async (context) => {
+      context.commit("logOut_session");
+      await router.push({ name: "SignIn" });
     },
   },
   modules: {},
