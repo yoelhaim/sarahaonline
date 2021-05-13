b72ae67f6492ecb6bfccbfe9ad6e8309cce9f3bc
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-05-13 00:47:20 +0000

    add auth including

--- public/saraha/src/store/index.js
+++ public/saraha/src/store/index.js
@@ -1,15 +1,12 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import Auth from "./models/auth";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  state: {},
+  mutations: {},
+  actions: {},
+  modules: { Auth },
+});
