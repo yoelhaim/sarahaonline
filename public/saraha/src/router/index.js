1076222f4019c5e2a5449c4aab4cc1a09f041550
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-05-13 23:28:36 +0000

    fixed probleme

--- public/saraha/src/router/index.js
+++ public/saraha/src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import store from "../store";
 import Home from "../views/Home.vue";
 import Authroute from "./authRoutes";
 import homeChalange from "./home";
@@ -13,6 +14,9 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+    meta: {
+      authRequired: true,
+    },
   },
   {
     path: "/about",
@@ -26,6 +30,39 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+router.afterEach((to) => {
+  Vue.nextTick(() => {
+    document.title = to.meta.title || "welcomme";
+  });
+});
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.authRequired)) {
+    if (store.getters.isLoggedIn) {
+      next();
+      return;
+    } else {
+      next({
+        name: "SignIn",
+        params: to.params,
+        query: {
+          nextPAtch: to.name,
+        },
+      });
+    }
+  } else if (to.matched.some((record) => record.meta.global)) {
+    next();
+    return;
+  } else {
+    if (store.getters.isLoggedIn) {
+      next({
+        name: "Home",
+      });
+    } else {
+      next();
+      return;
+    }
+  }
 });
 
 export default router;
