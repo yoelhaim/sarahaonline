7bcdb95677a0ffc78e87755be81f911b7e9352e8
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-05-13 00:46:37 +0000

    add json message and eror message

--- controller/userController.js
+++ controller/userController.js
@@ -1,32 +1,42 @@
 const { users } = require("../models");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const createError = require("http-errors");
+
 const addusers = async (req, res, next) => {
   try {
     const checkUsersEmail = await users.findAll({
       where: { email: req.body.email },
     });
     const checkUsersLink = await users.findAll({
-      where: { link_profile: req.body.email },
+      where: { link_profile: req.body.link_profile },
     });
-    if (checkUsersLink.length < 1) {
-      res.send("link false");
-    }
-    if (checkUsersEmail.length < 1) {
-      res.send("email false");
+    if (checkUsersLink.length > 0) {
+      res.json({
+        code: 1001,
+        message: "error link  already exit",
+      });
+    } else if (checkUsersEmail.length > 0) {
+      res.json({
+        code: 1002,
+        message: "error email  already exit",
+      });
     } else {
       const body = req.body;
       await users
         .create(body)
         .then((result) => {
-          res.send(result);
+          res.json({
+            message: "succefully create",
+            data: result,
+          });
         })
         .catch((err) => {
-          res.send(err);
+          next(createError.Forbidden(err));
         });
     }
   } catch (error) {
-    res.send(error);
+    next(createError.Forbidden(error));
   }
 };
 
