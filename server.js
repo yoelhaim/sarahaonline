d87f31baa94b6c65bd81226d1b1aaa03ce8c963b
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-05-15 01:32:24 +0000

    update in app

--- server.js
+++ server.js
@@ -2,14 +2,46 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const cors = require("cors");
-app.use([cors(), express.json(), express.urlencoded({ extended: false })]);
+const fileUpload = require("express-fileupload");
+const morgan = require("morgan");
+const path = require("path");
+///  routes
+
+const protectRoutes = require("./midelwaire/checklogin");
 // include routes
 const UserRoute = require("./route/userRoute");
 const MessageRoute = require("./route/messageRoute");
+
+///
+app.use([
+  cors(),
+  express.json(),
+  express.urlencoded({ extended: false }),
+  fileUpload({
+    createParentPath: true,
+  }),
+  morgan("dev"),
+]);
+
+app.get("/uploads/:id", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "/uploads/" + req.params.id));
+});
+//routes protected
+app.get("/msg/all/:id", protectRoutes);
 // fin include roues
 app.use("/auth", UserRoute);
 app.use("/msg", MessageRoute);
-
+//// vue
+app.use("*", (req, res, next) => {
+  if (req.baseUrl.trim() === "") {
+    req.baseUrl = "index.html";
+  }
+  res.sendFile(path.resolve(__dirname, `./public/${req.baseUrl}`), (err) => {
+    if (err) {
+      res.sendFile(path.resolve(__dirname, "./public/index.html"));
+    }
+  });
+});
 // error
 app.use(function (err, req, res, next) {
   const error = {
