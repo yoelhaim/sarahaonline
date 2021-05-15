ca7a3bd10563120d87163cdb18c30ee8f44d398c
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-05-15 01:34:57 +0000

    check login of logout

--- public/saraha/src/components/navBar/nav.vue
+++ public/saraha/src/components/navBar/nav.vue
@@ -6,32 +6,38 @@
           <!-- <v-app-bar-title>
             <h4 id="logo">إعترف لي</h4>
           </v-app-bar-title> -->
-          <v-img
-            src="https://i.pinimg.com/originals/41/28/2b/41282b58cf85ddaf5d28df96ed91de98.png"
-            width="60"
-            height="60"
-          />
+          <v-img src="@/assets/userlogo.png" width="60" height="60" />
         </router-link>
       </div>
 
       <v-spacer></v-spacer>
-      <router-link to="/">
-        <v-btn small text fab class="mr-2 btnColor">
-          <v-icon>mdi-facebook-messenger</v-icon>
-        </v-btn>
-      </router-link>
-      <router-link to="/mychalange">
+      <div v-if="!$store.getters.isLoggedIn">
+        <router-link to="/signup">
+          <v-btn small text class="mr-2 btnColor">
+            <v-icon>mdi-plus</v-icon>
+            إنشاء حساب
+          </v-btn>
+        </router-link>
+      </div>
+      <div v-else>
+        <router-link to="/">
+          <v-btn small text fab class="mr-2 btnColor">
+            <v-icon>mdi-facebook-messenger</v-icon>
+          </v-btn>
+        </router-link>
+        <!-- <router-link to="/mychalange">
         <v-btn small text fab class="mr-2 btnColor">
           <v-icon>mdi-puzzle-heart</v-icon>
         </v-btn></router-link
-      >
+      > -->
 
-      <v-btn small text fab class="mr-2 btnColor">
+        <!-- <v-btn small text fab class="mr-2 btnColor">
         <v-icon>mdi-youtube</v-icon>
-      </v-btn>
-      <v-btn small text fab class="mr-2 btnColor" @click="logout">
-        <v-icon>mdi-cogs</v-icon>
-      </v-btn>
+      </v-btn> -->
+        <v-btn small text fab class="mr-2 btnColor" @click="logout">
+          <v-icon>mdi-logout-variant</v-icon>
+        </v-btn>
+      </div>
     </v-app-bar>
   </div>
 </template>
